3fdd7e493a520fb9bfd30febfc5b191218a48ee4
Kyeong_seog <kimks9241@naver.com>
2023-10-24 13:49:21 +0900

    js name

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('Hi');
+console.log("Hi");
+
+// document.html <h2></h2>
+// html에 접근하기위해 설정한 div의 id를 등록 
+//
+
+let h2 = document.createElement("h2");
+
+let dom = document.getElementById("dom");
+
+h2.innerText = "My name is kyeong seog";
+
+dom.appendChild(h2);
+
